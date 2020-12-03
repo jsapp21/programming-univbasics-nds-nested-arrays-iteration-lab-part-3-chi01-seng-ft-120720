8bccb05dec22d4e6558953e2d4e5d26633ab4ccf
James Sapp <jameseisapp@gmail.com>
2020-12-03 22:14:18 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,7 +1,7 @@
 # src will be an Array of Arrays of Strings and Integers
 # Combine all Strings present in the AoA into a single value and return it
 
-
+# require 'pry'
 
 def join_nested_strings(src)
   row_index = 0 
@@ -19,9 +19,7 @@ def join_nested_strings(src)
     end 
     row_index += 1 
   end
-  
   single_string.join(' ')
-  
 end
 
 
